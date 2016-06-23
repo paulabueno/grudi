335e913f4f52c6679eccb8e3700757a85fa73123
paulabueno <paula@Thiagos-MacBook-Pro-2.local>
2016-06-23 17:15:22 -0300

    versão

--- js/creative.js
+++ js/creative.js
@@ -46,31 +46,5 @@
 
 })(jQuery); // End of use strict
 
-<<<<<<< HEAD
+
 // minhas alterações //
-
-// Get the modal
-var modal = document.getElementById('myModal-1');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn-1");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-1")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block"; }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-=======
->>>>>>> parent of bb5d7ca... new
